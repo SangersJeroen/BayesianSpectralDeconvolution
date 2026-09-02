826bcd504a50753fb92a3c1a5eba4bada3f1d56f
SangersJeroen <jeroen98.sangers@gmail.com>
2026-09-02 14:38:13 +0200

    fixed "__all__" method usage

--- src/bayspecdec/samplers/__init__.py
+++ src/bayspecdec/samplers/__init__.py
@@ -1,7 +1,7 @@
 from .metropolis import MetropolisState, RandomWalkMetropolis, metropolis_kernel_factory
 
 __all__ = [
-    MetropolisState,
-    RandomWalkMetropolis,
-    metropolis_kernel_factory,
+    "MetropolisState",
+    "RandomWalkMetropolis",
+    "metropolis_kernel_factory",
 ]
